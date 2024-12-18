edbb8db4e1cf0f4c3f09812c3e3c6f8350b09c09
SHUBHAM <shubhamraj1068@gmail.com>
2024-12-18 20:46:53 +0530

    Initialized repository

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <>
+        <div className="w-screen h-16 bg-[#040711]">
+            
+        </div>
+    </>
+  )
+}
+
+export default Navbar
